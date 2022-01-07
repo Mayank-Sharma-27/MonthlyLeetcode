71c81ff7492ac0d9a75002bfa7a217994784ad69
Mayank-sharma-27 <mayank.sharms2796@gmail.com>
2022-01-07 11:46:09 +0530

    Add solution for day7

---
+++ January/src/main/java/day7/Day7.java
@@ -0,0 +1,41 @@
+package day7;
+
+import java.util.*;
+
+public class Day7 {
+
+    Map<ListNode, Integer> map;
+    Random R;
+
+    public Day7(ListNode head) {
+        map = new HashMap();
+        R = new Random();
+        while (head != null) {
+            map.put(head, head.val);
+            head = head.next;
+        }
+    }
+
+    public int getRandom() {
+        List<ListNode> keys = new ArrayList<ListNode>(map.keySet());
+        return keys.get(R.nextInt(keys.size())).val;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
