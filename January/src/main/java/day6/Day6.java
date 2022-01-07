5d38251a8fdc5d6c72fa2d5b956e8ff8af6a77d2
Mayank-sharma-27 <mayank.sharms2796@gmail.com>
2022-01-07 12:49:02 +0530

    Sort array and use priority queue

--- January/src/main/java/day6/Day6.java
+++ January/src/main/java/day6/Day6.java
@@ -1,21 +1,37 @@
 package day6;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Day6 {
     public boolean carPooling(int[][] trips, int capacity) {
-        int[] count = new int[1001];
-        for (int i = 0; i < trips.length; i++) {
-            count[trips[i][1]] += trips[i][0];
-            count[trips[i][2]] -= trips[i][0];
-        }
 
-        int curr = 0;
-        for (int i : count) {
-            curr += i;
-            if (curr > capacity) {
-                return false;
+        Arrays.sort(trips, new Comparator<int[]>(){
+            public int compare(int[] i1, int[] i2) {
+                return i1[1] - i2[1];
             }
-        }
+        });
 
+        Queue<int[]> theQueue = new PriorityQueue<int[]>(new Comparator<int[]>(){
+            public int compare(int[] t1, int[] t2) {
+                return t1[2] - t2[2];
+            }
+        });
+
+        for (int i = 0; i < trips.length; i++) {
+            while (!theQueue.isEmpty() && theQueue.peek()[2] <= trips[i][1]) {
+                capacity += theQueue.peek()[0];
+                theQueue.poll();
+            }
+
+            capacity -= trips[i][0];
+            theQueue.offer(trips[i]);
+            if( capacity < 0 )
+                return false;
+        }
         return true;
+
     }
 }
